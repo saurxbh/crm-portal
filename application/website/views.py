89a7dc41dc4cec685ffa6ebc20048c56f2ef51ff
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-27 20:43:56 -0400

    Display Home Page if User is Logged In

--- application/website/views.py
+++ application/website/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+#
+
+def logout_user(request):
+    pass
