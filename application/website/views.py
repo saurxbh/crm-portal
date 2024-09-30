8070add4f5ddd796a103b2bb82178ef1a37bdc34
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-09-29 21:55:38 -0400

    Update Record

--- application/website/views.py
+++ application/website/views.py
@@ -80,5 +80,20 @@ def add_record(request):
     else:
         messages.success(request, "You must be logged in to view this page.")
         return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            record = form.save()
+            messages.success(request, 'Record updated successfully!')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect('home')
+    
+
         
 
